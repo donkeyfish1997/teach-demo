7c2df54696437d4967c2b140783a120e7b09db94
donkeyfish1997 <donkeyfish1997@gamil.com>
2021-10-28 09:00:39 +0800

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  data() {
+    const a = { b: { c: "hi" } };
+    return { a };
+  },
+};
+</script>
+<template>
+  <div>
+    <!-- 這麼寫可以 -->
+    {{ a.b.c }}
+    <!-- 這麼寫就會出錯 -->
+    {{ a?.b.c }}
+  
+
+  </div>
+</template>
+
